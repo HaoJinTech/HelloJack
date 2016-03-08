f1ccf7a5a8280b5049fe7983fd8df93a724a53a5
Andrew Patterson <ajypatterson@gmail.com>
2016-03-08 13:06:09 +0800

    Main
    This is for test

---
+++ HelloJack/HelloJack/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloJack
+//
+//  Created by Andrew Patterson on 3/8/16.
+//  Copyright © 2016 Andrew Patterson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, Jack!\n");
+    return 0;
+}
